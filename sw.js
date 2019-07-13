7d5f66b42faa265114ce541da0ee869a21d2269e
vkr <vkr1984@hotmail.com>
2019-07-13 17:24:57 -0500

    ui fixes

--- sw.js
+++ sw.js
@@ -32,3 +32,49 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+self.importScripts('push-notifications-controller.js');
+
+const pushNotificationTitle = 'mPortal';
+
+self.addEventListener('push', function (event) {
+    event.waitUntil(self.registration.showNotification(pushNotificationTitle, {
+        body: event.data.text(),
+        icon: '/img/tu.png'
+    }));
+});
+
+self.addEventListener('pushsubscriptionchange', function (event) {
+    const handlePushSubscriptionChangePromise = Promise.resolve();
+
+    if (event.oldSubscription) {
+        handlePushSubscriptionChangePromise = handlePushSubscriptionChangePromise.then(function () {
+            return PushNotificationsController.discardPushSubscription(event.oldSubscription);
+        });
+    }
+
+    if (event.newSubscription) {
+        handlePushSubscriptionChangePromise = handlePushSubscriptionChangePromise.then(function () {
+            return PushNotificationsController.storePushSubscription(event.newSubscription);
+        });
+    }
+
+    if (!event.newSubscription) {
+        handlePushSubscriptionChangePromise = handlePushSubscriptionChangePromise.then(function () {
+            return PushNotificationsController.retrievePublicKey().then(function (applicationServerPublicKey) {
+                return pushServiceWorkerRegistration.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: applicationServerPublicKey
+                }).then(function (pushSubscription) {
+                    return PushNotificationsController.storePushSubscription(pushSubscription);
+                });
+            });
+        });
+    }
+
+    event.waitUntil(handlePushSubscriptionChangePromise);
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+});
